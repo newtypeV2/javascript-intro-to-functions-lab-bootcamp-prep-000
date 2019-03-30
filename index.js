13b41dd06b6efffa32183b66b4ce87593da7a075
Gian Libunao <gianlibunao@gmail.com>
2019-03-30 19:04:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,19 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandMa(string){
+  if (string.toUpperCase() === )
 }
 
 
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
